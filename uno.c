fe52e4a069a433037ce20bd4b3199eb26258b3b1
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2022-05-06 17:41:00 +0200

    creato il loop di gioco nel main

--- uno.c
+++ uno.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
 #define SIZE_DECK 108
+#define STARTING_HAND_SIZE 7
 
 // per fare cancellare correttamente il terminale alla chiamata di system()
 // indipendentemente dal sistema operativo per cui il codice è compilato
@@ -32,6 +34,7 @@ int get_players();
 struct card *shuffle();
 struct card *find_empty_space(struct card *deck);
 struct card **init_players(struct card *deck, int *szDeck, int szPlayers);
+void turn(struct card *deck, int *szDeck, struct card **players, int *szPlayers, int *current_player, bool *rotation);
 
 int main()
 {
@@ -40,21 +43,32 @@ int main()
 
   deck = shuffle();
   int size_deck = SIZE_DECK;
-
+  /*
   for (int i = 0; i < size_deck; i++)
   {
     printf("f= %s\tc= %d\n", (deck + i)->face, (deck + i)->color);
   }
-
+  */
   int size_players = get_players();
   players = init_players(deck, &size_deck, size_players);
-
+  /*
   for (int i = 0; i < size_players; i++)
   {
     printf("mano del giocatore %d:\n", i);
-    for (int j = 0; j < 7; j++)
+    for (int j = 0; j < STARTING_HAND_SIZE; j++)
       printf("\t%s\t%d\n", (*(players + i) + j)->face, (*(players + i) + j)->color);
   }
+  */
+  int current_player = 0;
+  bool rotation = true; // true = sinistra; false = destra;
+
+  while (players + current_player)
+  // quando il gioco finisce il mazzo del current_player viene deallocato e portato a NULL
+  // quindi la condizione diventa falsa perchè (players + current_player) == NULL == 0
+  {
+    turn(deck, &size_deck, players, &size_players, &current_player, &rotation);
+  }
+
   return 0;
 }
 
@@ -163,7 +177,7 @@ struct card **init_players(struct card *deck, int *szDeck, int szPlayers)
     *(players + i) = (struct card *)malloc(sizeof(struct card) * (*szDeck)); // alloca lo spazio necessario
 
   for (int i = 0; i < szPlayers; i++) // pesca carte dal mazzo
-    for (int j = 0; j < 7; j++)
+    for (int j = 0; j < STARTING_HAND_SIZE; j++)
     {
       (*szDeck)--;
       ((*(players + i)) + j)->color = (deck + (*szDeck))->color;
@@ -171,4 +185,9 @@ struct card **init_players(struct card *deck, int *szDeck, int szPlayers)
     }
 
   return players;
+}
+
+void turn(struct card *deck, int *szDeck, struct card **players, int *szPlayers, int *current_player, bool *rotation)
+{
+  printf("\nhello\n");
 }
